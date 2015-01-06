ab6e26eca7673ec2bd13ea9ccc9f65f53824ada0
Andrew G <andrew@sfrent.net>
2015-01-06 09:56:23 -0800

    File upload cache

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -24,6 +24,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
     
   end
 
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
