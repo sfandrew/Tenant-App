5f66396c49434391f195d19085c2125de10bd669
Andrew G <andrew@sfrent.net>
2015-01-09 21:11:26 -0800

    Fixing upload

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,7 @@
 class Attachment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :attachable, :polymorphic => true, :dependent => :destroy
+	attr_accessor :filename_cache
 
 	mount_uploader :filename, AttachmentUploader
 	validates :filename, allow_blank: true, format:{
@@ -8,5 +9,9 @@ class Attachment < ActiveRecord::Base
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
 	# validates :content_meta, presence: :true
+
+	def filename_url_or_cached_url
+		filename_url || filename_cache
+	end
 
 end
