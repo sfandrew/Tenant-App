ff69581c97586ce770f092a0288e5f5f39198e3a
Andrew G <andrew@sfrent.net>
2015-06-11 16:44:19 -0700

    API

--- app/serializers/dynamic_forms_engine/dynamic_form_entry_serializer.rb
+++ app/serializers/dynamic_forms_engine/dynamic_form_entry_serializer.rb
@@ -1,22 +1,29 @@
 class DynamicFormsEngine::DynamicFormEntrySerializer < ActiveModel::Serializer
-  # attributes :id, :dynamic_form_type_id, :user_id, :in_progress, :created_at, :updated_at, :properties
-  attributes :application
+ 
+	def attributes
+	  	entry = {}
+	  	entry.merge!(application_name: object.dynamic_form_type.name, id: object.id, in_progress: object.in_progress, 
+	  									created: object.created_at, updated: object.updated_at)
 
-  def application
-  	entry = {}
-  	entry.merge!(application_name: object.dynamic_form_type.name, id: object.id, in_progress: object.in_progress, 
-  															created: object.created_at, updated: object.updated_at)
+	  	field_group_name = ''
+	  	object.properties.each_value do |value|
 
-  	object.properties.each_value do |value|
-  		if DynamicFormsEngine::DynamicFormField.field_with_null_value.include?(value[:type])
-  			next
-  		else 
-  			entry[value[:name]] = value[:value]	
-  		end
-  	end
-  	entry
-  	
-  end
+	  		if value[:type] == "field_group"
+	  			field_group_name = value[:name]
+	  			entry[field_group_name] = {}
+	  		elsif DynamicFormsEngine::DynamicFormField.field_with_null_value.include?(value[:type])
+	  			next
+	  		elsif value[:type] == "file_upload"
+	  			attachment = object.attachments.find { |file_upload| file_upload.content_meta == value[:id] }
+	  			entry[field_group_name][value[:name]] = attachment.filename.url unless attachment.nil?
+	  		else 
+	  			entry[field_group_name][value[:name]] = value[:value]
+	  		end
+
+	  	end
+	  	entry
+	  	
+	end
 
 
 end
