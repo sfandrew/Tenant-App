a749e4f3b3da984d3f3a26604439472017eb3074
Andrew G <andrew@sfrent.net>
2016-01-08 19:11:04 -0800

    Fixed attachments bug on rendering and deleting

--- app/decorators/controllers/dynamic_forms_engine/dynamic_form_entries_controller_decorator.rb
+++ app/decorators/controllers/dynamic_forms_engine/dynamic_form_entries_controller_decorator.rb
@@ -13,15 +13,15 @@ DynamicFormsEngine::DynamicFormEntriesController.class_eval do
     @contacts = Contact.where(id: Contactable.select(:contact_id).where(:contactable_type => "DynamicFormsEngine::DynamicFormEntry",:contactable_id => params[:id]))
   end
 
-  def autocomplete_feature
-    if !current_user.nil?
-        @contacts_hash = {}
-        current_user.contacts.each do |contact|
-          @contacts_hash[contact.first_name] = [contact.contact_type, contact.phone, contact.email, contact.company]
-        end
-        @contact_names = @contacts_hash.keys
-    end
-  end
+  # def autocomplete_feature
+  #   if !current_user.nil?
+  #       @contacts_hash = {}
+  #       current_user.contacts.each do |contact|
+  #         @contacts_hash[contact.first_name] = [contact.contact_type, contact.phone, contact.email, contact.company]
+  #       end
+  #       @contact_names = @contacts_hash.keys
+  #   end
+  # end
 
     def get_buildings
       uri = URI.parse('https://sfrent.net/tenant_app_api.json')
